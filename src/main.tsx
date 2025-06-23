bfc1ddbbf4e6e12ddea301272c2391d777806f11
MrzWolffy <pthitiakkarasak@gmail.com>
2025-06-23 18:06:10 +0700

    fix netlify 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { StatusPage } from './StatusPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
