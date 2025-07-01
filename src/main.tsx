ded45bff0552bb7c0779ea48780b66945b20ea1f
KongJen <supakorn.jen@gmail.com>
2025-07-01 19:21:25 +0700

    analyte

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import StatusPage from './StatusPage.tsx';
+import AnalytePage from './Analyte.tsx';
 
 createRoot(document.getElementById('root')!).render(
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/status" element={<StatusPage />} />
+          <Route path="/analyte" element={<AnalytePage />} />
         </Routes>
       </Router>
   </StrictMode>,
